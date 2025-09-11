eae3c025fe985aaafc81c8f65b8735bca3bcd945
ABAlosaimi <afalih.alosaimi@gmail.com>
2025-09-12 02:41:22 +0300

    added regrission & probability

--- frequncy/formula.py
+++ frequncy/formula.py
@@ -292,3 +292,31 @@ def correlation_coefficient(data, n):
     result = (n * sum_of_xy - sum_of_x * sum_of_y) / ((n * sum_of_x_squared - sum_of_x ** 2) * (n * sum_of_y_squared - sum_of_y ** 2)) ** 0.5
 
     return result
+
+# regression
+
+def regression_line(data, n, x):
+    sum_of_xy = 0 
+    sum_of_x = 0 
+    sum_of_y = 0 
+    sum_of_x_squared = 0 
+    sum_of_y_squared = 0 
+    
+    for i in range(len(data)):
+        sum_of_xy =+ data[i][0] * data[i][1]
+        sum_of_x =+ data[i][0]
+        sum_of_y =+ data[i][1]
+        sum_of_x_squared =+ data[i][0] ** 2
+        sum_of_y_squared =+ data[i][1] ** 2
+
+    a = (sum_of_y * sum_of_x_squared - sum_of_x * sum_of_xy) / (n * sum_of_x_squared - sum_of_x ** 2)
+    b = (n * sum_of_xy - sum_of_x * sum_of_y) / (n * sum_of_x_squared - sum_of_x ** 2)
+
+    result = a+b*x
+    
+    return result  
+
+# probability
+
+def probability(event, total_events):
+    return event / total_events
